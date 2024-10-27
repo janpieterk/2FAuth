84412197d5f32c88b647bdfe2c8a716aca522928
Bubka <858858+Bubka@users.noreply.github.com>
2024-10-27 09:44:10 +0100

    Rebuild assets with sourcemaps

--- public/build/assets/bus-D9XHlzlA.js
+++ public/build/assets/bus-D9XHlzlA.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD:public/build/assets/bus-D9XHlzlA.js
 import{a5 as e}from"./app-ClFPZZ4l.js";/*! 2FAuth version 5.3.2 - Copyright (c) 2024 Bubka - https://github.com/Bubka/2FAuth */const r=e({id:"bus",state:()=>({migrationUri:null,decodedUri:null,inManagementMode:!1,editedGroupName:null,username:null}),actions:{}});export{r as u};
-=======
-import{a5 as e}from"./app-CeOdx_TG.js";/*! 2FAuth version 5.3.0 - Copyright (c) 2024 Bubka - https://github.com/Bubka/2FAuth */const r=e({id:"bus",state:()=>({migrationUri:null,decodedUri:null,inManagementMode:!1,editedGroupName:null,username:null}),actions:{}});export{r as u};
-//# sourceMappingURL=bus-Sx3kCjbz.js.map
->>>>>>> aa4cb049 (Rebuild assets with sourcemaps):public/build/assets/bus-Sx3kCjbz.js
+//# sourceMappingURL=bus-D9XHlzlA.js.map
